37d40e26b7173e3e61008b54e1aaaf08b34c57e9
Adam Walker <adamwalker567123@gmail.com>
2020-08-01 01:17:56 -0700

    like every l33t hacker, dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import Main from "./Main";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
 import Footer from "./Footer";
-import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Main />
-        <Footer />
+        <ThemeProvider theme={darkTheme}>
+          <Route exact path="/">
+            <Header />
+            <Projects />
+          </Route>
+          <Footer />
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
